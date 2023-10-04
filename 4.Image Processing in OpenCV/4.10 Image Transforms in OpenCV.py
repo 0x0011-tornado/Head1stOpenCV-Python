d0d764a51d3a942afea86d505022eef98f663147
0xlitf <0xlitf@gmail.com>
2023-10-04 17:45:38 +0800

    ·

--- 4.Image Processing in OpenCV/4.10 Image Transforms in OpenCV.py
+++ 4.Image Processing in OpenCV/4.10 Image Transforms in OpenCV.py
@@ -10,14 +10,58 @@ matplotlib.use("QtAgg", force=True)
 plt.ion()
 
 
-def function():
+# cdf Cumulative Distribution Function 累积分布函数
+def cumulative_distribution_function():
     current_function_name = inspect.currentframe().f_code.co_name
     print("current_function_name:", current_function_name)
+
+    img = cv.imread('../data/wiki.png', 0)
+    hist, bins = np.histogram(img.flatten(), 256, [0, 256])
+    cdf = hist.cumsum()
+    cdf_normalized = cdf * float(hist.max()) / cdf.max()
+    plt.plot(cdf_normalized, color='b')
+    plt.hist(img.flatten(), 256, [0, 256], color='r')
+    plt.xlim([0, 256])
+    plt.legend(('cdf', 'histogram'), loc='upper left')
+    plt.show()
+
+    cdf_m = np.ma.masked_equal(cdf, 0)
+    cdf_m = (cdf_m - cdf_m.min()) * 255 / (cdf_m.max() - cdf_m.min())
+    cdf = np.ma.filled(cdf_m, 0).astype('uint8')
+
+    img2 = cdf[img]
+    plt.figure()
+    plt.plot(cdf, color='b')
+    plt.hist(img2.flatten(), 256, [0, 256], color='r')
+    plt.xlim([0, 256])
+    plt.legend(('cdf', 'histogram'), loc='upper left')
+    plt.show()
+
+
+def equalization():
+    img = cv.imread('../data/wiki.jpg', 0)
+    equ = cv.equalizeHist(img)
+    res = np.hstack((img, equ))  # stacking images side-by-side
+    cv.imwrite('res.png', res)
+
+    img = cv.imread('../data/tsukuba_l.png', 0)
+    # create a CLAHE object (Arguments are optional).
+    clahe = cv.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
+    cl1 = clahe.apply(img)
+    cv.imwrite('../data/clahe_2.jpg', cl1)
+
+    plt.figure()
+    plt.subplot(121)
+    plt.imshow(img)
+    plt.subplot(122)
+    plt.imshow(plt.imread('../data/clahe_2.jpg'))
+    plt.show()
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
 
-    function()
-
+    cumulative_distribution_function()
+    equalization()
+    
     sys.exit(app.exec())
--- 4.Image Processing in OpenCV/4.8 Contours in OpenCV.py
+++ 4.Image Processing in OpenCV/4.8 Contours in OpenCV.py
@@ -82,15 +82,66 @@ def moments(pic):  # ../data/white_rect_in_black.png #../4.8.0/star_filled@2x.pn
 
 
 # 5. Convex Hull
-def convex_hull():
-    pass
+def convex_hull_draw():
+    # 新建512*512的空白图片
+    img = np.zeros((512, 512, 3), np.uint8)
+    # 平面点集
+    pts = np.array([[200, 250], [250, 300], [300, 270], [270, 200], [120, 240]], np.int32)
+    pts = pts.reshape((-1, 1, 2))
+    # 绘制填充的多边形
+    cv.fillPoly(img, [pts], (255, 255, 255))
+
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    # 二值化
+    ret, thresh = cv.threshold(gray, 127, 255, cv.THRESH_BINARY)
+    # 图片轮廓
+    contours, hierarchy = cv.findContours(thresh, 2, 1)
+    cnt = contours[0]
+    # 寻找凸包并绘制凸包（轮廓）
+    hull = cv.convexHull(cnt)
+    print(hull)
+
+    length = len(hull)
+    for i in range(len(hull)):
+        cv.line(img, tuple(hull[i][0]), tuple(hull[(i + 1) % length][0]), (0, 255, 0), 2)
+
+    # 显示图片
+    cv.imshow('line', img)
+    cv.waitKey()
+
+
+# 5. Convex Hull
+def convex_hull(pic):
+    # 读取图片并转至灰度模式
+    img = cv.imread(pic, 1)
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+    # 二值化，取阈值为235
+    ret, thresh = cv.threshold(gray, 235, 255, cv.THRESH_BINARY)
+
+    # 寻找图像中的轮廓
+    contours, hierarchy = cv.findContours(thresh, 1, 2)
+
+    # 寻找物体的凸包并绘制凸包的轮廓
+    for cnt in contours:
+        hull = cv.convexHull(cnt)
+        length = len(hull)
+        # 如果凸包点集中的点个数大于5
+        if length > 5:
+            # 绘制图像凸包的轮廓
+            for i in range(length):
+                cv.line(img, tuple(hull[i][0]), tuple(hull[(i + 1) % length][0]), (0, 0, 255), 2)
+
+    cv.imshow('finger', img)
+    cv.waitKey()
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
 
-    contours_in_OpenCV()
-    moments('../4.8.0/electric.jpg')
-    convex_hull()
+    # contours_in_OpenCV()
+    # moments('../4.8.0/electric.jpg')
+    # convex_hull('../data/gesture.png')
+    convex_hull_draw()
 
     sys.exit(app.exec())
